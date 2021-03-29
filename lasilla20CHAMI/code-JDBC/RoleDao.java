c6b00d183840a4bba4f06d91fd008b5ea82db25e
JEONG CHA MI <lasilla20@naver.com>
2021-03-29 23:05:18 +0900

    Update RoleDao.java

--- lasilla20CHAMI/code-JDBC/RoleDao.java
+++ lasilla20CHAMI/code-JDBC/RoleDao.java
@@ -10,10 +10,10 @@ import kr.or.connect.jdbcexam.dto.Role;
 
 public class RoleDao {
 	private static String dburl = "jdbc:mysql://localhost:3306/connectdb";
-	private static String dbUser = "connectuser";
-	private static String dbpasswd = "connect123!@#";
+	private static String dbUser = "root";
+	private static String dbpasswd = "201911011";
 
-	public Role getRole(Integer roleId) {
+	public Role getRole(Integer roleId) {//정수 roleID를 입력받음
 		Role role = null;
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -23,13 +23,15 @@ public class RoleDao {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
 			String sql = "SELECT description,role_id FROM role WHERE role_id = ?";
+			//?에 해당하는 부분만 변경됨. 이걸 prepare 함수에서 설정해줄 수 있음
 			ps = conn.prepareStatement(sql);
+			//role이 int 이므로 setInt. ?가 1개 이므로 첫번째 물음표 = roleID 이다.
 			ps.setInt(1, roleId);
 			rs = ps.executeQuery();
 
 			if (rs.next()) {
-				String description = rs.getString(1);
-				int id = rs.getInt("role_id");
+				String description = rs.getString(1);//첫번째 컬럼을 꺼냄
+				int id = rs.getInt("role_id"); //컬럼 이름으로 꺼내도 괜찮음
 				role = new Role(id, description);
 			}
 		} catch (Exception e) {
