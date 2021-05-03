2bf698bc4a7e72ed7289c0d6cfef15bdb557aadf
Hyeji Lim <59350891+hyeji1221@users.noreply.github.com>
2021-05-03 18:42:42 +0900

    Add files via upload

---
+++ hyeji1221/Web App Development Scheduling Service1/code/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package kr.or.connect.diexam01;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class ApplicationConfig {
+	@Bean
+	public Car car(Engine e) {
+		Car c = new Car();
+		return c;
+	}
+	
+	@Bean
+	public Engine engine() {
+		return new Engine();
+	}
+}
