5ec42b343f7be142c43ca90a8c9908b7d2096ba5
JEONG CHA MI <lasilla20@naver.com>
2021-05-04 11:54:34 +0900

    Update Car.java

--- lasilla20CHAMI/3-Web App Development Scheduling Service1(Spring)/3-Code/Car.java
+++ lasilla20CHAMI/3-Web App Development Scheduling Service1(Spring)/3-Code/Car.java
@@ -23,12 +23,3 @@ Car c = new Car();
 c.setEngine( e );
 c.run();
 */
-
-/*
-1, 2 번째 줄을 Spring 컨테이너에게 맡기기 위해 설정파일에 다음과 같은 코드를 입력합니다.
-
-<bean id="e" class="kr.or.connect.diexam01.Engine"></bean>
-<bean id="car" class="kr.or.connect.diexam01.Car">
-	<property name="engine" ref="e"></property>
-</bean>
-*/
